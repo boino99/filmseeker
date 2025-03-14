a82ca0aa6c9c61c76f8a161dbfbefd9ffc524fa0
boino99 <adan.bp99@gmail.com>
2025-03-14 16:56:45 -0600

    Fixed loading button

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -37,7 +37,7 @@ const SearchBar = () => {
       {!isPending ? (
         <Button
           color="secondary"
-          className="w-60 m-auto"
+          className="min-w-52"
           title={"buscar"}
           onPress={() => handleSearch()}
         >
@@ -47,7 +47,7 @@ const SearchBar = () => {
         <button
           disabled
           type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+          className="relative text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
         >
           <svg
             aria-hidden="true"
